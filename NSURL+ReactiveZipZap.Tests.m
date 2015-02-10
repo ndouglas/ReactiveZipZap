aa5715d078e42f7f9be7c9a1561f7a520ee6502f
Nathan Douglas <ndouglas@devontechnologies.com>
2015-02-10 11:53:39 -0600

    Adds NSURL (ReactiveZipZap) tests.

--- NSURL+ReactiveZipZap.Tests.m
+++ NSURL+ReactiveZipZap.Tests.m
@@ -11,23 +11,31 @@
 #import "ReactiveZipZap.h"
 
 @interface NSURL_ReactiveZipZapTests : XCTestCase
-
+@property (copy, nonatomic, readwrite) NSURL *temporaryURL;
+@property (copy, nonatomic, readwrite) NSURL *ephemeralURL;
 @end
 
 @implementation NSURL_ReactiveZipZapTests
+@synthesize temporaryURL;
+@synthesize ephemeralURL;
 
 - (void)setUp {
 	[super setUp];
+    self.temporaryURL = [[NSURL rzz_temporaryURL] first];
+    self.ephemeralURL = [[NSURL rzz_ephemeralURL] first];
 }
 
 - (void)tearDown {
+    [[NSFileManager defaultManager] removeItemAtURL:self.temporaryURL error:NULL];
 	[super tearDown];
 }
 
-- (void)test {
-	/*
-		Run a test here.
-	*/
+- (void)testTemporaryURL {
+    XCTAssertTrue([[NSFileManager defaultManager] fileExistsAtPath:self.temporaryURL.path]);
+}
+
+- (void)testEphemeralURL {
+    XCTAssertTrue(![[NSFileManager defaultManager] fileExistsAtPath:self.ephemeralURL.path]);
 }
 
 @end
