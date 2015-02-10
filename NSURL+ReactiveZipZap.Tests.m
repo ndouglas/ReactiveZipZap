53aaac406b4483cb36d52df5622a9ce0d712bd08
Nathan Douglas <ndouglas@devontechnologies.com>
2015-02-10 15:14:54 -0600

    Small tests for NSURL (ReactiveZipZap).

--- NSURL+ReactiveZipZap.Tests.m
+++ NSURL+ReactiveZipZap.Tests.m
@@ -38,4 +38,32 @@
     XCTAssertTrue(![[NSFileManager defaultManager] fileExistsAtPath:self.ephemeralURL.path]);
 }
 
+- (void)testNamesOfExtendedAttributesWithError {
+	NSURL *URL = [NSURL fileURLWithPath:@(__FILE__)];
+    XCTAssertNotNil(URL);
+    NSError *error = nil;
+    NSArray *names = [URL rzz_namesOfExtendedAttributesWithError:&error];
+    XCTAssertNotNil(names);
+}
+
+- (void)testSetValueForExtendedAttributeWithNameError {
+	NSURL *URL = [NSURL fileURLWithPath:@(__FILE__)];
+    XCTAssertNotNil(URL);
+    NSError *error = nil;
+    NSString *testAttributeName = @"testAttributeName";
+    NSString *testAttributeValue = @"testAttributeValue";
+    NSArray *names = [URL rzz_namesOfExtendedAttributesWithError:&error];
+    if (!names) {
+        XCTAssertTrue([URL rzz_setValue:[testAttributeValue dataUsingEncoding:NSUTF8StringEncoding] forExtendedAttributeWithName:testAttributeName error:&error]);
+        names = [URL rzz_namesOfExtendedAttributesWithError:&error];
+    }
+    XCTAssertNotNil(names);
+    if ([names containsObject:testAttributeName]) {
+        XCTAssertTrue([URL rzz_removeExtendedAttributeWithName:testAttributeName error:&error]);
+    }
+    XCTAssertTrue([URL rzz_setValue:[testAttributeValue dataUsingEncoding:NSUTF8StringEncoding] forExtendedAttributeWithName:testAttributeName error:&error]);
+    XCTAssertEqualObjects([[NSString alloc] initWithData:[URL rzz_valueForExtendedAttributeWithName:testAttributeName error:&error] encoding:NSUTF8StringEncoding], testAttributeValue);
+    XCTAssertTrue([URL rzz_removeExtendedAttributeWithName:testAttributeName error:&error]);
+}
+
 @end
