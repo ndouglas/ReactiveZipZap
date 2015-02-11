434103a7816b476d82fa235a666a14fc2b6f32df
Nathan Douglas <ndouglas@devontechnologies.com>
2015-02-11 07:46:56 -0600

    Tidy.

---
+++ ZZArchiveEntry+ReactiveZipZap.h
@@ -0,0 +1,15 @@
+//
+//  ZZArchiveEntry+ReactiveZipZap.h
+//  ReactiveZipZap
+//
+//  Created by Nathan Douglas on 2/10/15.
+//  Released into the public domain.
+//  See LICENSE for details.
+//
+
+#import "RZZDefinitions.h"
+
+@interface ZZArchiveEntry (ReactiveZipZap)
+
+
+@end
---
+++ ZZArchiveEntry+ReactiveZipZap.m
@@ -0,0 +1,16 @@
+//
+//  ZZArchiveEntry+ReactiveZipZap.m
+//  ReactiveZipZap
+//
+//  Created by Nathan Douglas on 2/10/15.
+//  Released into the public domain.
+//  See LICENSE for details.
+//
+
+#import "ZZArchiveEntry+ReactiveZipZap.h"
+#import "ReactiveZipZap.h"
+
+@implementation ZZArchiveEntry (ReactiveZipZap)
+
+
+@end
